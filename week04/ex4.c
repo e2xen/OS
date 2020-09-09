5a9730ea9c9ab3e3c29d28de963d5248b78af75a
e2xen <67383731+e2xen@users.noreply.github.com>
2020-09-09 16:02:44 +0300

    Update ex4.c

--- week04/ex4.c
+++ week04/ex4.c
@@ -7,39 +7,46 @@
 
 int main(int argc, char *argv[], char *envp[]) {
     char inp[256];
-    while (1) {
-        fgets(inp, 256, stdin);
-        
-        char *cmd[16];
-        for (int i = 0; i < 16; i++)
-            cmd[i] = malloc(64*sizeof(char));
+    
+    printf(">> ");
+    fgets(inp, 256, stdin);
+    inp[strlen(inp)-1] = 0; // remove '\n' at the end
+    
+    char *cmd[16]; // array of arguments
+    for (int i = 0; i < 16; i++)
+        cmd[i] = malloc(64*sizeof(char));
 
-        int counter = 1;
-        char *p = strtok(inp, " ");
-        while (p != NULL) {
-            strcpy(cmd[counter], p);
-            p = strtok(NULL, " ");
+    int counter = 1;
+    cmd[0] = strtok(inp, " ");
+    while (cmd[counter-1] != NULL) {
+        cmd[counter] = strtok(NULL, " ");
 
-            if (++counter >= 16) {
-                break;
-            }
+        if (++counter >= 16) {
+            break;
         }
-
-        // for (int i = 0; i < counter; i++)
-        //     printf("%s|", cmd[i]);
-
-        if (!strcmp(cmd[counter], "&")) {
-            if (fork() == 0) {
-                printf("[%d]\n", getpid());
-                cmd[counter-1] = NULL;
-                execve("/bin/sh", cmd, envp);
-            }
-        } else {
-            cmd[counter+1] = NULL;
-            execve("/bin/sh", cmd, envp);
-        }
-
     }
+    counter--;
+
+    // for (int i = 0; cmd[i]; i++)
+    //      printf(" %s", cmd[i]);
+    // printf("end, counter = %d\n", counter);
+
+    if (!strcmp(cmd[counter-1], "&")) {
+        if (fork() == 0) {
+            printf("[%d]\n", getpid());
+            cmd[counter-1] = NULL;
+            execvp(cmd[0], cmd);
+            exit(0);
+        } else {
+          sleep(1);
+        }
+    } else {
+        //printf("parent [%d]\n", getpid());
+        cmd[counter] = NULL;
+        execvp(cmd[0], cmd);
+    }
+    
+
 
     return 0;
 }
